45cfa58502856222575aa2cc17c385acc40c016c
manzaa5555 <40601@prot.ac.th>
2025-03-10 15:19:08 +0700

    Add files via upload

---
+++ cell01/ex13/13.py
@@ -0,0 +1,7 @@
+first_name  = 'teerapong'
+last_name = 'chenman'
+whole_name = (first_name + ' ' + last_name)
+
+print("hey, what's your first name?", first_name)
+print("what's your last_name?", last_name)
+print("full name: ", whole_name)
